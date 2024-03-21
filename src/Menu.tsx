449d50007f12566133cbef4eedae1a8555474e64
RiverMadsen <rtaig@entrustsol.com>
2024-03-21 08:48:40 -0700

    Defined Menu and Icons

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,30 +1,42 @@
 // import React from 'react';
 import styles from './Menu.module.css';
+import { FaDrawPolygon } from "react-icons/fa";
+import { BsPencil } from "react-icons/bs";
+import { BsRecordCircle } from "react-icons/bs";
+import { LuMountainSnow } from "react-icons/lu";
+import { ImCompass } from "react-icons/im";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineHelp } from "react-icons/md";
+import { LuGlasses } from "react-icons/lu";
+import { FiShare2 } from "react-icons/fi";
+import { TiFolderOpen } from "react-icons/ti";
 
 interface MenuProps {
     onMenuClose: () => void;
 }
 const menuItems = [
-    { id: 0, name: "Take Area Offline" },
-    { id: 1, name: "Draw a Route" },
-    { id: 2, name: "Record a Route" },
-    { id: 3, name: "View Elevation Profile" },
-    { id: 4, name: "Navigate to Route" },
-    { id: 5, name: "Settings" },
-    { id: 6, name: "Help" },
-    { id: 7, name: "Geek Zone" },
+    { id: 0, name: "Open a Route", icon: <TiFolderOpen />, color:"nex-red"},
+    { id: 1, name: "Take Area Offline", icon: <FaDrawPolygon />, color:"nex-orange"},
+    { id: 2, name: "Draw a Route" , icon: <BsPencil />, color:"nex-yellow"},
+    { id: 3, name: "Record a Route" , icon: <BsRecordCircle />, color:"nex-green"},
+    { id: 4, name: "View Elevation Profile" , icon: <LuMountainSnow />, color:"nex-blue"},
+    { id: 5, name: "Navigate to Route" , icon: <ImCompass />, color:"nex-purple"},
+    { id: 6, name: "Share Route" , icon: <FiShare2 />, color:"nex-red"},
+    { id: 7, name: "Settings" , icon: <IoSettingsOutline />, color:"nex-orange"},
+    { id: 8, name: "Help" , icon: <MdOutlineHelp />, color:"nex-yellow"},
+    { id: 9, name: "Geek Zone" , icon: <LuGlasses />, color:"nex-green"},
 
 ]
 const Menu: React.FC<MenuProps> = ({ onMenuClose }) => {
     return (
         <>
 
-            <div className='absolute  z-1000 py-6 px-2 h-screen w-10/12 md:w-2/6 bg-gradient-to-r from-black via-black/90 to-black/70'>
-                <h2 className=' text-5xl'><span className='bg-cyan-500'>Geo</span><span className='text-green-500'>Nexus</span></h2>
-                <h4 className='mt-2 italic text-sm text-yellow-300'>Where Tech Meets Adventure!</h4>
+            <div className='font-sans absolute  z-1000 py-6 px-4 h-screen w-10/12 md:w-2/6 bg-gradient-to-r from-black via-black/90 to-black/70'>
+                <h2 className=' text-5xl'><span className='px-1 bg-nex-blue rounded-lg text-black'>Geo</span><span className='text-nex-green ml-1'>Nexus</span></h2>
+                <h4 className='mt-1 italic text-sm text-nex-dark-white'>Adventure Meets Tech</h4>
                 <div className="menuClose">
                     <button className="hover:text-transparent absolute top-0 right-0 m-4 p-1 bg-transparent" onClick={onMenuClose}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-yellow-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-nex-red border-2 rounded-md border-nex-yellow">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
@@ -34,8 +46,9 @@ const Menu: React.FC<MenuProps> = ({ onMenuClose }) => {
                         {
                             menuItems.map((item) => {
                                 return (
-                                    <li key={item.id} className="border-b-2 py-4">
-                                        <a className={styles.menuItemText} >{item.name}</a>
+                                    <li key={item.id} className="border-b-2 py-4 border-nex-dark-gray flex items-center ">
+                                        <div className={`text-${item.color} mr-2 text-2xl`}>{item.icon}</div>
+                                        <a className='text-nex-dark-white font-normal  ' >{item.name}</a>
                                     </li>
                                 )
                             })
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': [ 'Helvetica','Arial', 'sans-serif'],
+    },
     extend: {
       zIndex: {
         '100': '100',
@@ -27,6 +30,16 @@ module.exports = {
         'black': '#000000',
         'white': '#FFFFFF',
         'semitransparent': 'rgba(0, 0, 0, 0.5)',
+        'nex-blue': 'hsl(190, 100%, 50%)',
+        'nex-green': 'hsl(80, 100%, 50%)',
+        'nex-red': 'hsl(320, 100%, 50%)',
+        'nex-yellow': 'hsl(54, 100%, 50%)',
+        'nex-orange': 'hsl(32, 100%, 50%)',
+        'nex-purple': 'hsl(290, 100%, 53%)',
+        'nex-white': 'hsl(0, 100%, 100%)',
+        'nex-dark-white': 'hsl(0, 0%, 80%)',
+        'nex-gray': 'hsl(0, 0%, 50%)',
+        'nex-dark-gray': 'hsl(0, 0%, 30%)',
       },
     },
   },
